1569524c9495fa262d5f6968a5e986270d075351
Wesley-nfj <wesleyfonkem7@icloud.com>
2025-06-06 10:21:42 +0100

    Create Dice-Roller.py

---
+++ Dice-Roller.py
@@ -0,0 +1,17 @@
+import random
+def roll_dice():
+    return random.randint(1,6)
+
+while True:
+   user_input= input("\n Roll a dice? (yes or no):\n")
+   if user_input== "yes":
+      result= roll_dice()
+      print(f"You rolled a {result}")
+
+   elif user_input== "no":
+      print("BYE!")
+      break 
+
+   else:
+    print("Enter yes or no")
+    continue
