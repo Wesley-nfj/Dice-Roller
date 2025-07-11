c9cbf9f6c52d87b720f4fbcb136bd7fc46c17811
Wesley-nfj <wesleyfonkem7@icloud.com>
2025-07-12 00:00:03 +0100

    Added feature to count number of each face rolled

--- Dice-Roller.py
+++ Dice-Roller.py
@@ -1,14 +1,26 @@
 import random
+from collections import defaultdict
+
 def roll_dice():
     return random.randint(1,6)
+
+roll_count= defaultdict(int)
+total_roll =0
 #Ask user for input
 while True:
    user_input= input("\n Roll a dice? (yes or no):\n")
    if user_input== "yes":
       result= roll_dice()
+      roll_count[result] += 1
+      total_roll +=1
       print(f"You rolled a {result}")
 
    elif user_input== "no":
+       print("\n🎯 Roll Summary:")
+       print(f"Total rolls: {total_rolls}")
+       for face in range(1, 7):
+           count = roll_counts[face]
+           print(f"Face {face}: {count}")
       print("BYE!")
       break 
 
